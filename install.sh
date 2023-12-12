f0e19e27e68c6c7238847a42373a1e4f3ef2f30a
Maxim Savenkov <max_savenkov@mail.ru>
2023-12-12 15:32:37 +0100

    System independent install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 rm -rf target
-mvn install
+mvn install -P Linux,MacOS,"MacOS M1+",Windows
 rm ~/.m2/repository/dev/mv/engine/0.5.3/engine-0.5.3.pom
 cp installed.xml ~/.m2/repository/dev/mv/engine/0.5.3/engine-0.5.3.pom
